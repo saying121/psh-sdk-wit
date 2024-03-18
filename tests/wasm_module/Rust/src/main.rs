27512ccbbae94301a1ad8e9984273d12eb6b8f84
dearfl <i@flr.me>
2024-03-18 09:22:47 +0800

    update wasm component example

--- tests/wasm_module/Rust/src/main.rs
+++ tests/wasm_module/Rust/src/main.rs
@@ -1,8 +1,35 @@
 mod bindings;
-use bindings::*;
+
+use crate::bindings::profiling::{
+    perf::{
+        config::{Config, Cpu, Event, EventScope, ExtraConfig, HardwareEvent, Process},
+        counter::Counter,
+    },
+    system,
+};
 
 extern crate alloc;
 
 fn main() {
-    println!("{}", psh::profiling::system::os_version().unwrap()); 
+    println!("{:?}", system::os::get_distro_version());
+    println!("{:?}", system::os::get_kernel_version());
+    let config = Config {
+        event: Event::Hardware(HardwareEvent::CpuCycles),
+        scopes: vec![EventScope::User],
+        extra_config: ExtraConfig {
+            pinned: false,
+            exclusive: false,
+            inherit: false,
+            inherit_stat: false,
+            inherit_thread: false,
+            enable_on_exec: false,
+            remove_on_exec: false,
+        },
+    };
+    let counter = Counter::new(Process::Current, Cpu::Any, &config).unwrap();
+    counter.enable().unwrap();
+    println!("do something here...");
+    counter.disable().unwrap();
+    let stat = counter.stat().unwrap();
+    println!("Counter Stat: {:#?}", stat);
 }
